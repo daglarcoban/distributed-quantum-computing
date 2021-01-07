76f5aaf16dea6606a43e541f8e65f75c38c3bf1e
Daglar Coban <daglarcoban20@gmail.com>
2021-01-07 12:22:59 +0100

    add examples from QI

---
+++ cat_entangler.py
@@ -0,0 +1,132 @@
+import os
+from getpass import getpass
+from math import sqrt
+
+from quantuminspire.credentials import load_account, get_token_authentication, get_basic_authentication
+
+from qiskit.circuit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from qiskit import execute
+
+from quantuminspire.qiskit import QI
+
+QI_EMAIL = os.getenv('QI_EMAIL')
+QI_PASSWORD = os.getenv('QI_PASSWORD')
+QI_URL = os.getenv('API_URL', 'https://api.quantum-inspire.com/')
+
+def get_authentication():
+    """ Gets the authentication for connecting to the Quantum Inspire API."""
+    token = load_account()
+    if token is not None:
+        return get_token_authentication(token)
+    else:
+        if QI_EMAIL is None or QI_PASSWORD is None:
+            print('Enter email:')
+            email = input()
+            print('Enter password')
+            password = getpass()
+        else:
+            email, password = QI_EMAIL, QI_PASSWORD
+        return get_basic_authentication(email, password)
+
+if __name__ == '__main__':
+    authentication = get_authentication()
+    QI.set_authentication(authentication, QI_URL)
+    qi_backend = QI.get_backend('QX single-node simulator')
+    authentication = get_authentication()
+    QI.set_authentication(authentication, QI_URL)
+    qi_backend = QI.get_backend('QX single-node simulator')
+
+    q = QuantumRegister(5)
+    c = ClassicalRegister(5)
+    circuit = QuantumCircuit(q, c)
+
+    alpha = 1/sqrt(2)
+    beta = 1/sqrt(2)
+    circuit.initialize([alpha, beta], 0)
+
+    #Entangle qubit 2-5 (index 1-4)
+    circuit.h(q[1])
+    circuit.cx(q[1], q[2])
+    circuit.cx(q[2], q[3])
+    circuit.cx(q[3], q[4])
+
+    circuit.barrier()
+    circuit.cx(q[0], q[1])
+    circuit.measure(q[1], c[1])
+    circuit.barrier()
+
+    #Use classical measurement result of qubit 2 to control x gates
+    circuit.x(q[1]).c_if(c[1], 1)
+    circuit.x(q[2]).c_if(c[1], 1)
+    circuit.x(q[3]).c_if(c[1], 1)
+    circuit.x(q[4]).c_if(c[1], 1)
+
+    print(circuit.draw())
+    circuit.measure(q, c)
+
+    print("\nResult from the remote Quantum Inspire backend:\n")
+    qi_job = execute(circuit, backend=qi_backend, shots=256)
+    qi_result = qi_job.result()
+    # Print the full state counts histogram
+    histogram = qi_result.get_counts(circuit)
+    print('State\tCounts')
+    [print('{0}\t{1}'.format(state, counts)) for state, counts in histogram.items()]
+
+    # Print the full state probabilities histogram
+    probabilities_histogram = qi_result.get_probabilities(circuit)
+    print('\nState\tProbabilities')
+    [print('{0}\t\t{1}'.format(state, val)) for state, val in probabilities_histogram.items()]
+
+
+    print("\nResult from the local Qiskit simulator backend:\n")
+    backend = BasicAer.get_backend("qasm_simulator")
+    job = execute(circuit, backend=backend, shots=1024)
+    result = job.result()
+    print(result.get_counts(circuit))
+    q = QuantumRegister(5)
+    c = ClassicalRegister(5)
+    circuit = QuantumCircuit(q, c)
+
+    alpha = 1/sqrt(2)
+    beta = 1/sqrt(2)
+    circuit.initialize([alpha, beta], 0)
+
+    #Entangle qubit 2-5 (index 1-4)
+    circuit.h(q[1])
+    circuit.cx(q[1], q[2])
+    circuit.cx(q[2], q[3])
+    circuit.cx(q[3], q[4])
+
+    circuit.barrier()
+    circuit.cx(q[0], q[1])
+    circuit.measure(q[1], c[1])
+    circuit.barrier()
+
+    #Use classical measurement result of qubit 2 to control x gates
+    circuit.x(q[1]).c_if(c[1], 1)
+    circuit.x(q[2]).c_if(c[1], 1)
+    circuit.x(q[3]).c_if(c[1], 1)
+    circuit.x(q[4]).c_if(c[1], 1)
+
+    print(circuit.draw())
+    circuit.measure(q, c)
+
+    print("\nResult from the remote Quantum Inspire backend:\n")
+    qi_job = execute(circuit, backend=qi_backend, shots=256)
+    qi_result = qi_job.result()
+    # Print the full state counts histogram
+    histogram = qi_result.get_counts(circuit)
+    print('State\tCounts')
+    [print('{0}\t{1}'.format(state, counts)) for state, counts in histogram.items()]
+
+    # Print the full state probabilities histogram
+    probabilities_histogram = qi_result.get_probabilities(circuit)
+    print('\nState\tProbabilities')
+    [print('{0}\t\t{1}'.format(state, val)) for state, val in probabilities_histogram.items()]
+
+
+    print("\nResult from the local Qiskit simulator backend:\n")
+    backend = BasicAer.get_backend("qasm_simulator")
+    job = execute(circuit, backend=backend, shots=1024)
+    result = job.result()
+    print(result.get_counts(circuit))
