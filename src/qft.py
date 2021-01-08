31dd0097203c8d8dd98e973b7c9468b44da7681c
Mohammed Boky <mohammedboky@outlook.com>
2021-01-08 12:58:07 +0100

    qft

---
+++ src/qft.py
@@ -0,0 +1,75 @@
+import os
+from getpass import getpass
+from numpy import pi
+# importing Qiskit
+from qiskit import QuantumCircuit, execute, Aer, IBMQ
+from qiskit.providers.ibmq import least_busy
+from qiskit.tools.monitor import job_monitor
+from qiskit.visualization import plot_histogram, plot_bloch_multivector
+
+from quantuminspire.credentials import load_account, get_token_authentication, get_basic_authentication
+
+from qiskit.circuit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from qiskit import execute, BasicAer
+
+from quantuminspire.qiskit import QI
+
+QI_EMAIL = os.getenv('QI_EMAIL')
+QI_PASSWORD = os.getenv('QI_PASSWORD')
+QI_URL = os.getenv('API_URL', 'https://api.quantum-inspire.com/')
+
+
+def get_authentication():
+    """ Gets the authentication for connecting to the Quantum Inspire API."""
+    token = load_account()
+    if token is not None:
+        return get_token_authentication(token)
+    else:
+        if QI_EMAIL is None or QI_PASSWORD is None:
+            print('Enter email:')
+            email = input()
+            print('Enter password')
+            password = getpass()
+        else:
+            email, password = QI_EMAIL, QI_PASSWORD
+        return get_basic_authentication(email, password)
+
+#functions from qiskit website
+
+def qft_rotations(circuit, n):
+    if n == 0: # Exit function if circuit is empty
+        return circuit
+    n -= 1 # Indexes start from 0
+    circuit.h(n) # Apply the H-gate to the most significant qubit
+    for qubit in range(n):
+        # For each less significant qubit, we need to do a
+        # smaller-angled controlled rotation:
+        circuit.cu1(pi/2**(n-qubit), qubit, n)
+
+def swap_registers(circuit, n):
+    for qubit in range(n//2):
+        circuit.swap(qubit, n-qubit-1)
+    return circuit
+
+def qft(circuit, n):
+    """QFT on the first n qubits in circuit"""
+    qft_rotations(circuit, n)
+    swap_registers(circuit, n)
+    return circuit
+
+
+if __name__ == '__main__':
+    q = QuantumRegister(5)
+    c = ClassicalRegister(5)
+    qc = QuantumCircuit(q,c)
+    qft(qc, 5)
+    qc.measure(q,c)
+    backend = BasicAer.get_backend("qasm_simulator")
+    job = execute(qc, backend=backend, shots=1024)
+    result = job.result()
+
+    print(result.get_counts(qc))
+
+    print(qc.draw())
+
+
