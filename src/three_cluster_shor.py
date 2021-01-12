454cdef4899a4f627ef547d5ab6c7db2c25fc004
Daglar Coban <daglarcoban20@gmail.com>
2021-01-12 15:57:38 +0100

    add three cluster shor code

--- src/three_cluster_shor.py
+++ src/three_cluster_shor.py
@@ -2,12 +2,16 @@ import numpy as np
 import os
 from getpass import getpass
 
+from qiskit.visualization import circuit_drawer
 from quantuminspire.credentials import load_account, get_token_authentication, get_basic_authentication
 
 from qiskit.circuit import QuantumRegister, ClassicalRegister, QuantumCircuit
 from qiskit import execute, BasicAer
 
 from quantuminspire.qiskit import QI
+
+from src.cat_disentangler import get_cat_disentangler
+from src.cat_entangler import get_cat_entangler
 
 QI_EMAIL = os.getenv('QI_EMAIL')
 QI_PASSWORD = os.getenv('QI_PASSWORD')
@@ -54,14 +58,149 @@ if __name__ == '__main__':
     authentication = get_authentication()
     QI.set_authentication(authentication, QI_URL)
     qi_backend = QI.get_backend('QX single-node simulator')
-    N = 9
-    c = ClassicalRegister(N)
-    q = QuantumRegister(N)
-    circuit = QuantumCircuit(q, c)
+    c_a = ClassicalRegister(4)
+    q_a = QuantumRegister(4)
+    c_b = ClassicalRegister(4)
+    q_b = QuantumRegister(4)
+    c_c = ClassicalRegister(4)
+    q_c = QuantumRegister(4)
+    cluster_reg = [q_a, q_b, q_c]
+    circuit_a = QuantumCircuit(q_a, c_a)
+    circuit_b = QuantumCircuit(q_b, c_b)
+    circuit_c = QuantumCircuit(q_c, c_c)
+
+    # Channel qubits are q_a[3], q_b[3], q_c[3]
 
     alpha = 0
     beta = 1
-    circuit.initialize([alpha, beta], q[0])
-    #circuit + toffoli(circuit, 6, 7, 8, q)  Toffoli Test
+    circuit_a.initialize([alpha, beta], q_a[0])
+
+    circuit = circuit_a + circuit_b + circuit_c
+
+    circuit = circuit.compose(get_cat_entangler(2), [q_a[0], q_a[3], q_b[3]])
+    circuit.cx(q_b[3], q_b[0])
+    circuit = circuit.compose(get_cat_disentangler(2), [q_a[0], q_a[3], q_b[3]])
+
+    circuit.barrier() # until first cnot between 1 and 4
+
+    circuit = circuit.compose(get_cat_entangler(2), [q_a[0], q_a[3], q_c[3]])
+    circuit.cx(q_c[3], q_c[0])
+    circuit = circuit.compose(get_cat_disentangler(2), [q_a[0], q_a[3], q_c[3]])
+
+    circuit.barrier() # until second cnot between 1 and 7
+
+    circuit.h(q_a[0])
+    circuit.h(q_b[0])
+    circuit.h(q_c[0])
+
+    circuit.cx(q_a[0], q_a[1])
+    circuit.cx(q_a[0], q_a[2])
+    circuit.cx(q_b[0], q_b[1])
+    circuit.cx(q_b[0], q_b[2])
+    circuit.cx(q_c[0], q_c[1])
+    circuit.cx(q_c[0], q_c[2])
+
+    circuit.barrier()  # until ERROR BLOCK
+
+    ### ERROR BLOCK --- START
+    random_bit = np.random.choice([0,1,2])
+    random_cluster = np.random.choice([0,1,2])
+    error = np.random.random(1)
+
+    if  error >= 0.66:
+        circuit.z(cluster_reg[random_cluster][random_bit])
+    elif error >= 0.33 and error < 0.66:
+        circuit.x(cluster_reg[random_cluster][random_bit])
+    else:
+        circuit.y(cluster_reg[random_cluster][random_bit])
+    ### ERROR BLOCK --- END
+
+    circuit.barrier() # after ERROR BLOCK
+
+    circuit.cx(q_a[0], q_a[1])
+    circuit.cx(q_a[0], q_a[2])
+    circuit.cx(q_b[0], q_b[1])
+    circuit.cx(q_b[0], q_b[2])
+    circuit.cx(q_c[0], q_c[1])
+    circuit.cx(q_c[0], q_c[2])
+
+    circuit + toffoli(circuit, 1, 2, 0, q_a)
+    circuit + toffoli(circuit, 1, 2, 0, q_b)
+    circuit + toffoli(circuit, 1, 2, 0, q_c)
+
+    circuit.barrier() # until h gates
+
+    circuit.h(q_a[0])
+    circuit.h(q_b[0])
+    circuit.h(q_c[0])
+
+    circuit.barrier() # until non local stuff
+
+    circuit = circuit.compose(get_cat_entangler(2), [q_a[0], q_a[3], q_b[3]])
+    circuit.cx(q_b[3], q_b[0])
+    circuit = circuit.compose(get_cat_disentangler(2), [q_a[0], q_a[3], q_b[3]])
+
+    circuit.barrier() # until first cnot between 1 and 4
+
+    circuit = circuit.compose(get_cat_entangler(2), [q_a[0], q_a[3], q_c[3]])
+    circuit.cx(q_c[3], q_c[0])
+    circuit = circuit.compose(get_cat_disentangler(2), [q_a[0], q_a[3], q_c[3]])
+
+    circuit.barrier() # until non local toffoli
+
+
+    ### NON LOCAL TOFFOLI GATES --- START
+    circuit.h(q_a[0])
+
+    circuit = circuit.compose(get_cat_entangler(2), [q_c[0], q_c[3], q_a[3]])
+    circuit.cx(q_a[3], q_a[0])
+    circuit = circuit.compose(get_cat_disentangler(2), [q_c[0], q_c[3], q_a[3]])
+
+    circuit.tdg(q_a[0])
+
+    circuit = circuit.compose(get_cat_entangler(2), [q_b[0], q_b[3], q_a[3]])
+    circuit.cx(q_a[3], q_a[0])
+    circuit = circuit.compose(get_cat_disentangler(2), [q_b[0], q_b[3], q_a[3]])
+
+    circuit.t(q_a[0])
+
+    circuit = circuit.compose(get_cat_entangler(2), [q_c[0], q_c[3], q_a[3]])
+    circuit.cx(q_a[3], q_a[0])
+    circuit = circuit.compose(get_cat_disentangler(2), [q_c[0], q_c[3], q_a[3]])
+
+    circuit.tdg(q_a[0])
+
+    circuit = circuit.compose(get_cat_entangler(2), [q_b[0], q_b[3], q_a[3]])
+    circuit.cx(q_a[3], q_a[0])
+    circuit = circuit.compose(get_cat_disentangler(2), [q_b[0], q_b[3], q_a[3]])
+
+    circuit.t(q_c[0])
+    circuit.t(q_a[0])
+
+    circuit = circuit.compose(get_cat_entangler(2), [q_b[0], q_b[3], q_c[3]])
+    circuit.cx(q_c[3], q_c[0])
+    circuit = circuit.compose(get_cat_disentangler(2), [q_b[0], q_b[3], q_c[3]])
+
+    circuit.h(q_a[0])
+    circuit.t(q_b[0])
+    circuit.tdg(q_c[0])
+
+    circuit = circuit.compose(get_cat_entangler(2), [q_b[0], q_b[3], q_c[3]])
+    circuit.cx(q_c[3], q_c[0])
+    circuit = circuit.compose(get_cat_disentangler(2), [q_b[0], q_b[3], q_c[3]])
+    ### NON LOCAL TOFFOLI GATES --- END
 
     print(circuit.draw())
+
+    # circuit_drawer(circuit, './circuit.png')
+
+    for i in range(3):
+        for j in range(4):
+            circuit.measure(circuit.qregs[i][j], circuit.cregs[i][j])
+
+    qi_job = execute(circuit, backend=qi_backend, shots=256)
+    qi_result = qi_job.result()
+    histogram = qi_result.get_counts(circuit)
+    print('State\tCounts')
+    [print('{0}\t{1}'.format(state, counts)) for state, counts in histogram.items()]
+
